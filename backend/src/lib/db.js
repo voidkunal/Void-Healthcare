c9862fe295c20801eeb7f28a57d92376bb3c8cdf
voidkunal <voidkunal@Kunals-MacBook-Air.local>
2024-12-13 18:04:53 +0530

    db section changes

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,10 +1,25 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//  try {
+//  const conn = await mongoose. connect(process.env.MONGODB_URI) ;
+//  console.log('MongoDB connected: ${conn.connection.host}');
+//  } catch(error) {
+//  console. log ("MongoDB connection error:", error);
+//  }
+//  };
+
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
- try {
- const conn = await mongoose. connect(process.env.MONGODB_URI) ;
- console.log('MongoDB connected: ${conn.connection.host}');
- } catch(error) {
- console. log ("MongoDB connection error:", error);
- }
- };
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1); // Exit process with failure
+    }
+};
